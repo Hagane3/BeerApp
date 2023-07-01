630cb6ecf851c83c4990cbc701bae91ceeb9e6b0
Konrad <konslo96@gmail.com>
2023-07-01 20:32:24 +0200

    feat: Add Navbar, add basic style variables, change html head icon

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
 
 const RootLayout = () => {
   return (
-    <div>
-      Navbar
-      <Outlet />
-    </div>
+    <>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+    </>
   );
 };
 
