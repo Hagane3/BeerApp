e16d4e4f9370cff259fcfce0b704a173b6c20f1a
Konrad <konslo96@gmail.com>
2023-07-02 11:31:23 +0200

    feat: Add scrollRestoration func when route changes

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
+import { ScrollRestoration } from "react-router-dom";
 
 const RootLayout = () => {
   return (
@@ -8,6 +9,7 @@ const RootLayout = () => {
       <main>
         <Outlet />
       </main>
+      <ScrollRestoration />
     </>
   );
 };
