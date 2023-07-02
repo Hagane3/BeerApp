73d518fec90af76ae021ee73d260534304ddaa6f
Konrad <konslo96@gmail.com>
2023-07-02 09:20:52 +0200

    feat: Add BeerItem and BeerList components

---
+++ src/components/BeerItem/BeerItem.tsx
@@ -0,0 +1,34 @@
+import classes from "./BeerItem.module.scss";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import LoadingSpinner from "../UI/LoadingSpinner/LoadingSpinner";
+
+interface Props {
+  beer: {
+    id: number;
+    name: string;
+    tagline: string;
+    image_url: string;
+  };
+}
+
+const BeerItem = ({ beer }: Props) => {
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+  // console.log(beer);
+  return (
+    <div className={classes.root}>
+      <h3>{beer.name}</h3>
+      <LoadingSpinner isLoading={!isImageLoaded} color="#fdd71f" />
+      <img
+        src={beer.image_url}
+        className={isImageLoaded ? classes.active : classes.inActive}
+        alt="beer image"
+        onLoad={() => setIsImageLoaded(true)}
+      />
+      <p>{beer.tagline}</p>
+      <Link to={`details/${beer.id}`}>Check details</Link>
+    </div>
+  );
+};
+
+export default BeerItem;
---
+++ src/components/BeerList/BeerList.tsx
@@ -0,0 +1,40 @@
+import BeerItem from "../BeerItem/BeerItem";
+import classes from "./BeerList.module.scss";
+import useFetch from "../../hooks/useFetch";
+import { PER_PAGE } from "../../constants";
+import { useState } from "react";
+import LoadingSpinner from "../UI/LoadingSpinner/LoadingSpinner";
+import Pagination from "../UI/Pagination/Pagination";
+import useMoreData from "../../hooks/useMoreData";
+
+const BeerList = () => {
+  const [page, setPage] = useState(1);
+  const { data, loading, error } = useFetch(
+    `https://api.punkapi.com/v2/beers?per_page=${PER_PAGE}&page=${page}`
+  );
+  console.log(data);
+  const { hasMoreData } = useMoreData({ data, perPage: PER_PAGE });
+
+  return (
+    <div className={classes.root}>
+      {error && <p className={classes.error}>{error.message}</p>}
+      {loading && !error ? (
+        <div className={classes.wrapper}>
+          <LoadingSpinner isLoading={loading} color="#fdd71f" />
+        </div>
+      ) : (
+        data &&
+        data.map((beer: any) => {
+          return <BeerItem key={beer.id} beer={beer} />;
+        })
+      )}
+      <Pagination
+        changePageHandler={setPage}
+        actualPage={page}
+        hasMoreData={hasMoreData}
+      />
+    </div>
+  );
+};
+
+export default BeerList;
